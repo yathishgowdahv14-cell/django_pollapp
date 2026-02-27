f394582eb6444ef0379704f363fae2ab64da3f96
unknown <yathishgowdahv14-cell>
2026-02-27 15:51:06 +0530

    py

--- djangotutorial/mysite/settings.py
+++ djangotutorial/mysite/settings.py
@@ -115,3 +115,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+INSTALLED_APPS = [
+    "polls.apps.PollsConfig",
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+]
